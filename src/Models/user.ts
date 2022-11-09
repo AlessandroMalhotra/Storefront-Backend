d40294f36a86a7e547dd32563deb72ef77f85ac1
Alessandro Malhotra <alessandro244@outlook.com>
2022-11-09 18:18:28 +0000

    Update User.ts
    Updated user.ts

--- src/Models/user.ts
+++ src/Models/user.ts
@@ -1,12 +1,51 @@
 import Client from '../Database/database';
 
 type User = {
-    id: Number;
-    firstname: string;
-    lastname: string;
-    password: string;
+  id: Number;
+  firstName: string;
+  lastName: string;
+  password: string;
 }
 
 class UserAccounts {
-    
+  async index(): Promise<User[]> {
+    try {
+      const connection = await Client.connect();
+      const sql = 'SELECT * FROM users';
+      const user = await connection.query(sql);
+      console.log(user);
+      connection.release();
+      return user.rows;
+    } catch (error) {
+      throw new Error(`Cannot get weapons ${error}`);
+    }
+  }
+
+  async show(id: string): Promise<User[]> {
+    try {
+      const connection = await Client.connect();
+      const sql = 'SELECT * FROM users WHERE id = ($1)';
+      const user = await connection.query(sql, [id]);
+      console.log(user);
+      connection.release();
+      return user.rows;
+    } catch (error) {
+      throw new Error(`Cannot get weapons ${error}`);
+    }
+  }
+
+  async create(u: User): Promise<User[]> {
+    try {
+      const connection = await Client.connect();
+      const sql = 'INSERT INTO user (firstName, lastName, password) VALUES ($1, $2, $3)';
+      const user = await connection.query(sql,[u.firstName, u.lastName, u.password]);
+      console.log(user);
+      connection.release();
+      return user.rows;
+    } catch (error) {
+      throw new Error(`Cannot insert product into database ${error}`);
+    }
+  }
 }
+
+export {User, UserAccounts};
