b57fb8e6f5b109144aa39ee48c2da94f50d9bb4d
Alessandro Malhotra <alessandro244@outlook.com>
2022-11-11 16:10:44 +0000

    Added endpoint to get user

--- src/Routes/API/userAPI.ts
+++ src/Routes/API/userAPI.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import { UserAccounts } from '../../Models/user';
 
 const user = express.Router();
+const userAccount = new UserAccounts();
+
+user.get('/users', async (req: express.Request, res: express.Response) => {
+    try {
+        const users = await userAccount.index();
+        res.send(users);
+    } catch (error) {
+        res.send(`Cannot get users due to ${error}.`);
+    }
+})
 
 export default user;
